52f99ac2a127c399b863d1d1fc80a952c5f8d9f6
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2024-09-06 13:03:52 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FASTAPI_TAILWIND
 test for FastAPI-tailwind package
+
+https://github.com/THEGOLDENPRO/fastapi-tailwind
---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from fastapi_tailwind import tailwind
+from contextlib import asynccontextmanager
+
+static_files = StaticFiles(directory = "static")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    process = tailwind.compile(static_files.directory + "/output.css")
+    yield 
+    process.terminate()
+
+
+app = FastAPI(lifespan=lifespan)
+app.mount("/static", static_files, name = "static")
+
+@app.get("/")
+async def index():
+    return FileResponse("./index.html")
+
+@app.get("root")
+async def root():
+    return {"msg": "Hello world"}
